cfd293e778e52542dad5225ef0028863e4ce1bf4
Rex Flores <Rexgflores@gmail.com>
2021-01-12 18:28:18 -0800

    added Constructor objects to appropriate JS pages

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(github){
+    super(name, id, email)
+    
+    getGithub()
+    
+    // getRole() TODO:override to Engineer  
+    }
+}
+
+module.exports = Engineer
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,10 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+class Manager extends Employee {
+    constructor(officeNum) {
+    super(name,id,email)
+    this.officeNum = officeNum
+    // getRole() TODO:Overide to "manager"
+    
+    }
+}
