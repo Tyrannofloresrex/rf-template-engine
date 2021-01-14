2dc8522fac84ee762e63da8b489fae161c2737f8
Rex Flores <Rexgflores@gmail.com>
2021-01-13 18:57:34 -0800

    Added methods to proper classes, created employee prompt, specific employee class prompts, prompt to intialize render, and prompt to loop intitial questions to make multiple employees in one session

--- Develop/app.js
+++ Develop/app.js
@@ -4,108 +4,138 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
+
+const currentEmployees = []
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function employeeQuestions(){
+function mainQuestions(){
     inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your id#?"
-        }, 
-        {
-            type: "input",
-            name: "email",
-            message: "What is your E-mail @ddress?"
-        },
-        {
-            type: "list",
-            name: "role",
-            message: "What is your role with the company?",
-            choices: ["Manager", "Engineer", "Intern"],
-        }
-
-
-    ]).then ((info)=>{
-        switch (info.role) {
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id#?"
+    }, 
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's E-mail @ddress?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role with the company?",
+        choices: ["Manager", "Engineer", "Intern"],
+    }
+        ]).then ((info) =>{
+            switch (info.role) {
             case "Manager":
                 getOfficeNum(info);
                 break;
-
             case "Engineer":
-                getGithub();
+                getGithub(info);
                 break;
-
             case "Intern":
-                getSchool();
+                getSchool(info);
                 break;
-            
             default:
                 break;   
         }
-})
-} employeeQuestions()
-// TODO:Ask user if they want to make another account
-function getOfficeNum(answer){
+    }) 
+} 
+mainQuestions()
+
+
+function getOfficeNum(info){
     inquirer.prompt ([
         {
             type: "input",
             name: "officeNum",
-            message: "What is your office number?"
+            message: "What is this manager's office number?"
         }
     ]).then(answer => {
-        console.log(data)
-        console.log(answers)
-        const newManager = new Manager (name, id, email, officeNum)
+        
+        const newManager = new Manager (info.name, info.id, info.email, answer.officeNum)
+        addEmployee(newManager)
+        
     }) 
     return
 }
 
-function getGithub(){
+function getGithub(info){
     inquirer.prompt([
         {
             type: "input",
             name: "github",
-            message: "What is your Git Hub ID?"
+            message: "What is this engineer's Git Hub ID?"
         }
-    ]).then (answers => {
-        console.log(answers)
-        Engineer.github = answers.github
-        Engineer.prototype.role = "Engineer"
+    ]).then (answer => {
+        
+        const newEngineer = new Engineer (info.name, info.id, info.email, answer.github)
+        addEmployee(newEngineer)
     })
     return
 }
 
-function getSchool(){
+function getSchool(info){
     inquirer.prompt([
         {
             type: "input",
             name: "school",
-            message: "What school are you enrolled at?"
+            message: "What school is this intern enrolled at?"
         }
-    ]).then (answers => {
-        console.log(answers)
-        Intern.school = answers.school
-        Intern.prototype.role = "Intern"
+    ]).then (answer => {
+        
+        const newIntern = new Intern (info.name, info.id, info.email, answer.school)
+        addEmployee(newIntern)
     })
     return
 }
 
-// TODO:function getRole() {
+function askMore() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "confirm",
+            message: "Do you want to add another employee? If not, we will now render your employees."
+        }
+    ]).then (confirm => {
+        if (confirm.confirm === true) {
+            
+            return mainQuestions()
+            
+        } fs.writeFile("employees.html", render(currentEmployees), (err) => {
+        
+            if (err) {
+                throw err
+            } else {
+                console.log("Success!")
+            }
+        })
+           
+        
+    })
+}
 
-// }
+function addEmployee (employee) {
+    currentEmployees.push(employee)
+    askMore()
+}
+
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
